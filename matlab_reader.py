d2e1342193a92fb32f8a0a94faedd37f4bcda202
lucas <llestandi@u-bordeaux.fr>
2018-06-22 16:33:30 +0200

    new matlab reader routine with TTSVD

--- matlab_reader.py
+++ matlab_reader.py
@@ -11,22 +11,28 @@ import h5py
 from SHOPOD import SHOPOD
 from high_order_decomposition_method_functions import unit_mass_matrix_creator
 from plot_error_tucker import plot_error_tucker
+from plot_error_tt import plot_error_tt
+from TT_SVD2 import TT_SVD2
 
 
-def matlab_file_reduction(file_name, error_evaluation='no'):
+def matlab_file_reduction(file_name, error_evaluation=False):
     '''
-    Etapes pour lire un fichier de matlab V 7.3 
+    Etapes pour lire un fichier de matlab V 7.3
     '''
 
     f=h5py.File(file_name,'r')
     data=f[list(f.keys())[0]]
     data=np.array(data)
-    Mass_matrix=unit_mass_matrix_creator(data)
-    U=SHOPOD(data,Mass_matrix)
-    
-    if error_evaluation=='yes':
-        plot_error_tucker(U,data,1, 'Error vs compression rate',
+    # Mass_matrix=unit_mass_matrix_creator(data)
+    # U=SHOPOD(data,Mass_matrix)
+    U=TT_SVD2 (data)
+
+    if error_evaluation:
+        # plot_error_tucker(U,data,1, 'Error vs compression rate',
+        #                   output_variable_name='Matlab_compression_file')
+        plot_error_tt(U,data,1, 'Error vs compression rate',
                           output_variable_name='Matlab_compression_file')
     return U
-    
 
+if __name__=='__main__':
+    matlab_file_reduction("exp_data/Exemple_1.mat",error_evaluation=True)
