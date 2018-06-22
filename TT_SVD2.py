c7edca052b5b3902fed8c4bb2ae62b0cd7d21a4a
diego_britez <diego-luis.britez-gonzalez@etu.u-bordeaux.fr>
2018-06-22 13:50:37 +0200

    TT_SVD2 We added TT_SVD2 function, is the SVD as POD in l2

---
+++ TT_SVD2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 13:19:41 2018
+
+@author: diego
+"""
+import numpy as np
+from TSVD import TSVD
+from TensorTrain import TensorTrain
+from scipy.sparse import diags
+from POD import POD2 as POD
+
+def TT_SVD2(F, eps=1e-8, rank=100):
+    """
+    Returns the decomposed form of a Tensor in the Tensor Train format by 
+    using the TT-SVD decomposition method described by I. V. Oseledets. \n 
+    **Parameters** \n
+    F= numpy.ndarray type. N dimentional tensor. \n
+    eps= maximal error expected in each decomposition. \n
+    rank= integer type . Maximal number of rank in each decomposed linear 
+    network.\n
+    **Returns** \n
+    G= Tensor Train class element. \n
+    
+    **Exemple** \n
+
+    F=np.arange(720) \n
+
+    F=F.reshape([8,9,10]) \n
+
+    G=TT_SVD(F, rank=5) \n
+    
+    
+    """
+    tshape=F.shape
+    
+    dim=len(tshape)
+    r=[1]
+    C=F
+    G=[]
+    for i in range(dim-1):
+       aux=r[i]*tshape[i]
+       mass1=np.ones(aux)
+       mass1=diags(mass1)
+       Csize=C.size
+       C=C.reshape(aux,int(Csize/aux))
+       mass2=np.ones(int(Csize/aux))
+       mass2=diags(mass2)
+       if rank!=100:
+           u,sigma,v=TSVD(C, rank=rank)
+       else:
+           u,sigma,v=POD(C, mass1,mass2,tol=eps)
+       
+       new_rank=sigma.shape[0]
+       r.append(new_rank)
+       G.append(u.reshape(r[i],tshape[i],r[i+1]))
+       C=sigma@v.T
+    G.append(C)
+    
+    Gdshape=list(G[dim-1].shape)
+    Gdshape.append(1)
+    
+    G[dim-1]=G[dim-1].reshape(Gdshape)
+    return TensorTrain(G)
