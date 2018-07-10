c249f2b8eeb47f74d5753b7ae501e29d58143f76
diego_britez <diego-luis.britez-gonzalez@etu.u-bordeaux.fr>
2018-07-10 14:35:35 +0200

    Modifications in POD algorithm and Mass Matrices file
    The class DiaMatrix was created, that is a reprepresentation of the mass
    matrix. It can be reprepresented either as ndarray object or as a sparse.
    
    The methods for a DiaMatrix object and a Matrix(ndarray) multiplication was
    also defined. In this operation the operator @ is used, the operation is
    defined with de DiaMatrix as the first element of the operation(left) and the
    ndarray object as de second element of the operation(rigth).
    
    Other methods such inverse, transpose(use less in a diagonal) and the square
    root where also defined.
    
    Another class that groups together all the DiaMatrix of a system in a list
    of Diamatrix object was also created with the name of MassMatrices. For the
    moment it has no methods but another property that gives the shape of the
    problem.
    
    The POD routine, now can handle the problem with the mass matrices Mx and Mt
    expressed as sparce or a vector(as numpy.ndarray). The data of the mass
    matrices is not longer an obligatory parameter. For defect the DiaMatrix are
    goint to be unitaries vectors (equivalent to the SVD solution).

---
+++ pydecomp/core/MassMatrices.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 13 16:17:19 2018
+
+@author: Diego Britez
+"""
+
+import numpy as np
+from scipy.sparse import diags
+import scipy.sparse
+import sys
+sys.path.append('/media/diego/OS/Users/Diego Britez/Documents/stage/pydecomp_V2/pydecomp/utils')
+import integrationpoints
+
+class DiaMatrix:
+    """
+    This class is created to simplify operations with mass matrices. Mass
+    matrices can be defined either as a sparse matrix, as a list or as 
+    a numpy.ndarray.
+    This class will unify the matmul operation for any of this formats. 
+    """
+    def __init__(self,M):
+        self.M=M
+    def __matmul__(self,Matrix):
+        if type(self.M)==scipy.sparse.dia.dia_matrix:
+            result=self.M@Matrix
+        elif (type(self.M)==np.ndarray):     
+            result=Matrix*self.M[:,np.newaxis]
+        elif type(self.M)==list:
+            M1=np.array(self.M)
+            result=Matrix*M1[:,np.newaxis]
+        return result
+    def inv(self):
+        """
+        This fonction will return the inverse of mass matrix as an DiaMatrix
+        object
+        """
+        #possible_mass_matrix_format=[list, scipy.sparse.dia.dia_matrix,
+                                # numpy.ndarray]
+        
+        list_or_array=[list, np.ndarray]
+        
+        if type(self.M)==scipy.sparse.dia.dia_matrix:
+            Maux=self.M
+            Maux=Maux.diagonal()
+            inv=1/Maux
+            inv=diags(inv)
+            inv=DiaMatrix(inv)
+        if type(self.M) in list_or_array:
+            inv=1/self.M
+            inv=DiaMatrix(inv)
+        return inv
+    
+    def sqrt(self):
+        """
+        Returns the square root of the input mass matrix
+        """
+        reponse=np.sqrt(self.M)
+        reponse=DiaMatrix(reponse)
+        
+        return reponse
+    
+    def transpose(self):
+        """
+        Returns the transpose of a MassMatrix as an MassMatrix object
+        """
+        reponse=DiaMatrix(self.M.T)
+        return reponse
+#------------------------------------------------------------------------------
+"""
+class MassMatrices:
+    
+    Parameters: \n
+    List of DiaMatrix objects.
+    
+    def __init__(self,DiaMatrix_list):
+        self.DiaMatrix_list=DiaMatrix_list
+        self.shape=[x.size for x in DiaMatrix_list]
+"""        
+
+#------------------------------------------------------------------------------
+
+        
+def mass_matrices_creator(X, sparse=False, integration_method='trapeze'):
+    """
+    Returns a list of   sparse diagonals matrices with the integration points
+    for the trapezoidal integration method (mass matrices) from a list of
+    cartesian grid points.\n
+
+    **Parameter**\n
+    X: list of Cartesian grids vectors. \n
+    sparse: if False, the data is going to be expresed as ndarray vector if 
+    True as sparse vectors.
+    integration_method: Default method 'trapeze'. Simpson option is not yet 
+    avaible.
+    **Return**
+    M:list of mass matrices (integration points for trapeze integration method)
+    as sparse.diag type elements. \n
+
+    **Example**
+    import high_order_decomposition_method_functions as hf \n
+    import numpy as np \n
+    X1=np.array([ 0.  ,  0.25,  0.5 ,  0.75,  1.  ]) \n
+    X2=np.array([ 0. ,  0.2,  0.4,  0.6,  0.8,  1. ]) \n
+    X3=np.array([ 0.        ,  0.33333333,  0.66666667,  1.        ]) \n
+    X=[] \n
+    X.append(X1) \n
+    X.append(X2) \n
+    X.append(X3) \n
+    M=hf.mass_matrices_creator(X) \n
+    print(M) \n
+        [<5x5 sparse matrix of type '<class 'numpy.float64'>' \n
+        with 5 stored elements (1 diagonals) in DIAgonal format>, \n
+        <6x6 sparse matrix of type '<class 'numpy.float64'>' \n
+        with 6 stored elements (1 diagonals) in DIAgonal format>, \n
+        <4x4 sparse matrix of type '<class 'numpy.float64'>' \n
+        with 4 stored elements (1 diagonals) in DIAgonal format>] \n
+    """
+
+    dim=len(X)
+    tshape=[]
+
+    a="""X's elements must be either numpy.array or list type elements"""
+    b="""X's elements must be vectors"""
+
+    for i in range(dim):
+        if type(X[i])==list:
+            X[i]=np.array(X[i])
+        aux=np.array([0])
+        if type(X[i])!= type(aux):
+            raise ValueError(a)
+        if (len(X[i].shape)>1):
+            raise ValueError(b)
+
+        aux=X[i].size
+        tshape.append(aux)
+
+    M=integrationpoints.IntegrationPoints(X,dim,tshape)
+    M=M.IntegrationPointsCreation()
+    if sparse:
+        for i in range (dim):
+            M[i]=diags(M[i])
+   
+    return M        
+   
+def matricize_mass_matrix(dim,i,M,sparse=True):
+    """
+    Returns the equivalent mass matrices of a matricized tensor.\n
+    **Parameters** \n
+    dim= number of dimentions of the problem. \n
+    i = dimension number to arange the function. \n
+    M= MassMatrices object element.
+    **Return** \n
+    :math:`M_{x}` =
+    The mass matrix  of the *"i"* dimention.\n
+    :math:`M_{t}` =
+    The equivalent mass matrix  of n-1 dimentions.\n
+
+    **Definition**\n
+    Be
+    :math:`M=(M_{1},M_{2}...,M_{i},..,M_{d})` a list with
+    :math:`M_{i}`
+    the  matrix of each dimention.\n
+    Be *"i"* the dimention selected dimention to rearange the mass list\n
+    The result of the function will return:\n
+    :math:`M_{x}=M_{i}`\n
+    :math:`M_{t}=M_{1} \otimes M_{2} \otimes...M_{i-1} \otimes M_{i+1} \otimes..
+    \otimes M_{d}`
+    """
+    #copy the list of mass matrices to M2
+    M2=M.DiaMatrix_list[:]
+    #moving the actual indice to the first order
+    M2.insert(0,M2.pop(i))
+
+    Mt=M2[1]
+    Mx=M2[0]
+ 
+    if sparse:
+        var=2
+        while var<=dim-1:
+            Mt=scipy.sparse.kron(Mt,M2[var],format='dia')
+            var=var+1
+    if not sparse:
+        var=2
+        while var<=dim-1:
+            Mt=scipy.kron(Mt,M2[var],format='dia')
+            var=var+1
+        return Mx,Mt      
--- pydecomp/core/POD.py
+++ pydecomp/core/POD.py
@@ -9,98 +9,106 @@ from scipy.sparse import diags
 import scipy.sparse
 import numpy as np
 from scipy.linalg import norm
-from core.tensor_algebra import truncate_modes
+from tensor_algebra import truncate_modes
+from MassMatrices import DiaMatrix
+import timeit
+#import MassMatrix
 
-def POD(F, Mx, Mt, tol=1e-17, rank=-1):
+def POD(F, Mx=[], Mt=[], tol=1e-10, rank=-1):
     """
     This function decompose a matrix as the productphi * diag(sigma) * A^t
     excuting the  POD method. Here the problem is treated as an apparent 2
     dimentions problem.
     *Parameters:*
-        Mx=scipy.sparse diagonal matrix with the integration points as the
-        diagonal elements for the trapezoidal method.
-        Mt=scipy.sparse diagonal matrix with the integration points as the
-        diagonal elements for the trapezoidal method, this matrix must have
-        only one principal diagonal.
         F= Array like, Matrix with the values normaly aranged in [0]
         dimention for space and [1] dimention for time if the case.
+        
+        Mx=numpy.ndarray, list or scipy.sparse diagonal matrix with t
+        he integration points as thediagonal elements for the trapezoidal 
+        method. If there is no input, solution equivalent to the SVD method 
+        is going to be generated.
+        Mt=ion points as thediagonal elements for the trapezoidal 
+        method. If there is no input, solution equivalent to the SVD method 
+        is going to be generated.
         tol= maximal tolerance for the eigenvalues.
         rank= maiximal number of modes to be taken as long the maximal
         tolerance is not reached.
     *Returns:*
-
         phi= Array like, modes of the decomposition in R[0]
         sigma=array like
         A= Array like, modes of the decomposition in R[1]
 
     *@todo* Improve efficiency of mass matrix handling
     """
-    #Verification if Mx has the scipy.sparse diagonal matrix format
-    a="""
-    Mx must be scipy.sparse diagonal type element, for more information read
-    scipy.sparse.diags documentation.
-    """
-    if type(Mx)!= scipy.sparse.dia.dia_matrix:
-        raise ValueError(print(a))
-    b="""
-    Mt must be scipy.sparse diagonal type element, for more information read
-    scipy.sparse.diags documentation.
-    """
-    #Verification if Mt has the scipy.sparse diagonal matrix format
-    if type(Mt)!= scipy.sparse.dia.dia_matrix:
-        raise ValueError(print(b))
-    c="""
-    Mt has to be 1D dimention with 'offsets=0' scipy.sparse.diags type element,
-    for more information read scipy.sparse.diags documentation.
-    """
-
-    #Verification if Mt is single diagonal sparse matrix
-    if Mt.offsets!=0:
-        raise ValueError(print(c))
-
+    start=timeit.default_timer()
     tshape=F.shape
+    #Creating mass matrices for l2 vectorial space if there is no mass matrices
+    #declared
+    if Mx==[]:
+        Mx=np.ones(tshape[0])
+    if Mt==[]:
+        Mt=np.ones(tshape[1])
+    mx=DiaMatrix(Mx)
+    mt=DiaMatrix(Mt)
+   
     Transposed_POD=False
     if tshape[1]>tshape[0]:
         F=F.T
-        Mx, Mt = Mt, Mx
-        Transposed_POD=True
-
-    C=build_correlation(F, Mx, Mt)
-
+        mx.M, mt.M = mt.M, mx.M
+        Transposed_POD=True 
+    C=build_correlation(F, mx, mt)
     Lambda , U =np.linalg.eigh(C)
     # Reversing order
     Lambda = Lambda[::-1]
     U=U[::,::-1]
-
     Lambda, U=truncate_modes(Lambda,tol,rank,U)
-    sigma=diags(np.sqrt(Lambda))
-
-    Mtsqinv=inv(np.sqrt(Mt))
-    A=Mtsqinv.T@U
-    phi=(F@Mt@A)@inv(sigma)
-
-    if Transposed_POD:
-        A,phi=phi,A
-
+    sigma=np.sqrt(Lambda)
+    
+    #Mtsq is has the square root of the Mt elements
+    A=(((mt.sqrt()).inv()).transpose())@U
+    
+    if type(mx.M)==scipy.sparse.dia.dia_matrix:
+        phi=(F@mt.M@A)
+        #Now we normalise phi
+        #phi=the operation phi[:,np.newaxis] allows to multiply a matrix to a 
+        # a vector simulating product of a matrix with a diagonal matrix
+        phi=phi*(1/sigma)
+        if Transposed_POD:
+            A,phi=phi,A
+    else:
+        phi=F*mt.M@A
+        phi=phi*(1/sigma)
+        if Transposed_POD:
+            A,phi=phi,A
+    stop=timeit.default_timer()
+    print(stop-start)
     return phi, sigma, A
 
-def build_correlation(F,Mx,Mt):
+def build_correlation(F,mx,mt):
+    
     """
     This function creates the C matrix of correlation in the POD method
     """
-    C=np.sqrt(Mt)@F.T@Mx@F@np.sqrt(Mt).T
+    if type(mx.M)==scipy.sparse.dia.dia_matrix:
+        C=mt.sqrt()@F.T@mx.M@F@np.sqrt(mt.M.T)
+    
+        
+    else:
+        #C=mt.sqrt()@F.T*mx.M@F*(np.sqrt(mt.M.T))
+        
+        C1=mt.sqrt()@F.T
+        C2=mx@F
+        C=C1@C2
+        MTsqrt=mt.sqrt()
+        Mtsqrt_transpose=MTsqrt.transpose()
+        C=Mtsqrt_transpose@C.T
+        C=np.transpose(C)
+       
+        
+        #C=C*(np.sqrt(mt.M.T))
     return C
 
-def inv(Mtsq):
-    """
-    This function allows to  invert an diagonal matrix in scypy.sparse.diags
-    format
-    """
-    Mtsq=Mtsq.diagonal()
-    Mtsqinv=1/Mtsq
-    Mtsqinv=diags(Mtsqinv)
-    return Mtsqinv
-
+"""
 if __name__=="__main__":
     print("\n Testing POD with random matrix and Identity weights\n")
     n,m=5,6
@@ -109,6 +117,7 @@ if __name__=="__main__":
     Mt=diags(np.ones(m))
     phi, sigma, A=POD(F,Mx,Mt)
     print("phi :\n {}\n sigma:\n {} \n A:\n {}".format(phi, sigma, A))
-    F_approx = phi@sigma@A.T
+    F_approx = phi*sigma@A.T
     err=np.linalg.norm(F_approx-F)
     print("\n Should be small : {}".format(err))
+"""
